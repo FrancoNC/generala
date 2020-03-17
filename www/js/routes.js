09991be2faed0dea958743e81afd4c73a11cb969
netkito <franco.n.calandra@gmail.com>
2020-03-17 19:31:02 -0300

    estado inicial

---
+++ www/js/routes.js
@@ -0,0 +1,28 @@
+
+var routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+  {
+    path: '/about/',
+    url: './pages/about.html',
+  },
+
+  {
+    path: '/wf7/',
+    url: 'https://framework7.io',
+  },
+
+    {
+    path: '/anotador/',
+    url: 'anotador.html',
+  },
+
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+
+];
