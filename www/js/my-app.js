06c3a0809a64ceded7c4a5d0abf7c655a8e34fa2
netkito <franco.n.calandra@gmail.com>
2020-03-18 12:46:49 -0300

    Nueva version

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -29,9 +29,10 @@ var app = new Framework7({
 
 var mainView = app.views.create('.view-main');
 
-var nomjug1 = "";
-var nomjug2 = "";
-
+var nomJug1 = "";
+var nomJug2 = "";
+var total1 = 0;
+var total2 = 0;
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
@@ -56,5 +57,84 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
     console.log(e);
     $$('#jug1').text(nomjug1);
     $$('#jug2').text(nomjug2);
+/*
+    $$('#volver').on('click', function () {
+      nomjug1 = "";
+      nomjug2 = "";
+      $$('#j1').val("");
+      $$('#j2').val("");
+      for(var j = 0; j < 2; j++){
+        for (var i = 0; i < ; i++) {
+          $$('#'+j+i).text("-");
+        }
+      }
+    })
+*/
+    $$('#limpiar').on('click', function () {
+      for(var j = 1; j < 3; j++){
+        for (var i = 1; i < 12; i++) {
+          $$("#j"+j+"_"+i).text("-");
+        };
+      }
+      $$('#t1').text("0");
+      $$('#t2').text("0");
+    })
+
+    function sumar1(n){
+      total1 += n;
+      $$('#t1').text(total1);
+    }
+    function sumar2(n){
+      total2 += n;
+      $$('#t2').text(total2);
+    }
+
+/////////////////////////////////
+$$('#j1_1').on('click', function () {
+  app.dialog.create({
+    title: 'Cantidad de dados',
+    //text: 'Dialog with vertical buttons',
+  buttons: [
+    {
+      text: '1 dado',
+      onClick: tocar("j1_1", "1"),
+    },
+    {
+      text: '2 dados',
+      onClick: tocar("j1_1", "2"),
+    },
+    {
+      text: '3 dados',
+      onClick: tocar("j1_1", "3"),
+    },
+    {
+      text: '4 dados',
+      onClick: tocar("j1_1", "4"),
+    },
+    {
+      text: '5 dados',
+      onClick: tocar("j1_1", "5"),
+    },
+    {
+      text: 'tachar',
+      onClick: tocar("j1_1", "X"),
+    },
+    {
+      text: 'Cancelar',
+    },
+  ],
+  verticalButtons: true,
+  }).open();
+});
+/////////////////////////////////
+function tocar(iden, index){
+  if(index == 1){
+    $$('#'+iden).text(index);
+  }
+  else if(index == 2){
+  $$('#'+iden).text(index);
+}
+}
+
 
 })
